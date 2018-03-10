c1fc73c13d662b7496ce3a62b350de836b573eb3
micahgrande <34119790+micahgrande@users.noreply.github.com>
2018-03-10 15:02:44 -0800

    Create BalancedTree.java

---
+++ BalancedTree.java
@@ -0,0 +1,92 @@
+/*  Micah Joseph Grande
+    cssc0900
+*/
+
+package data_structures;
+
+import java.util.Iterator;
+import java.util.TreeMap;
+
+public class BalancedTree<K extends Comparable<K>,V> implements DictionaryADT<K,V>{
+	
+	private TreeMap<K,V> brt;
+	
+	public BalancedTree() {
+		brt = new TreeMap<K,V>();
+	}
+
+	// Returns true if the dictionary has an object identified by
+	// key in it, otherwise false.
+	public boolean contains(K key) {
+		return brt.containsKey(key);
+	}
+
+	// Adds the given key/value pair to the dictionary.  Returns 
+	// false if the dictionary is full, or if the key is a duplicate.
+	// Returns true if addition succeeded.
+	public boolean add(K key, V value) {
+		if(brt.containsKey(key)) return false;
+		brt.put(key, value);
+		return true;
+	}
+
+	// Deletes the key/value pair identified by the key parameter.
+	// Returns true if the key/value pair was found and removed,
+	// otherwise false.
+	public boolean delete(K key) {
+		if(brt.remove(key) == null) return false;
+		return true;
+		
+	}
+
+	// Returns the value associated with the parameter key.  Returns
+	// null if the key is not found or the dictionary is empty.
+	public V getValue(K key) {
+		return brt.get(key);
+	}
+
+	// Returns the key associated with the parameter value.  Returns
+	// null if the value is not found in the dictionary.  If more 
+	// than one key exists that matches the given value, returns the
+	// first one found. 
+	public K getKey(V value) {
+		for(K key : brt.keySet()) 
+			if(((Comparable<V>)brt.get(key)).compareTo(value) == 0) return key;
+		return null;
+	}
+
+	// Returns the number of key/value pairs currently stored 
+	// in the dictionary
+	public int size() {
+		return brt.size();
+	}
+
+	// Returns true if the dictionary is at max capacity
+	public boolean isFull() {
+		return false;
+	}
+
+	// Returns true if the dictionary is empty
+	public boolean isEmpty() {
+		return brt.isEmpty();
+	}
+
+	// Returns the Dictionary object to an empty state.
+	public void clear() {
+		brt.clear();
+	}
+
+	// Returns an Iterator of the keys in the dictionary, in ascending
+	// sorted order.  The iterator must be fail-fast.
+	public Iterator keys() {
+		return brt.keySet().iterator();
+	}
+
+	// Returns an Iterator of the values in the dictionary.  The
+	// order of the values must match the order of the keys. 
+	// The iterator must be fail-fast. 
+	public Iterator values() {
+		return brt.values().iterator();
+	}
+
+	}
